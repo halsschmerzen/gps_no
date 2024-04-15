2248614075a3779f5fb6fce3eb8f335e4d596249
halsschmerzen <bastiansteampl@gmail.com>
2024-04-15 14:09:38 +0200

    Zeichnet schwarze Box um Nodes - muss verbessert werden

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Hardgecodeete Nodes
+// Können je nach Bedarf geändert werden
+// Eventuell füge ich eine Funktion hinein die nach Klick die Nodes bestimmt
+const nodes = [
+    [110, 240],
+    [220, 100],
+    [400, 50],
+    [650, 100]
+];
 
 // Speichert den Container div als Konstante
 const container = document.getElementById("container");
@@ -15,14 +24,47 @@ function drawKaestchen(xPos, yPos) {
     // Werf es in den Container div hinein!
     container.appendChild(kaestchen);
 }
-// Hardgecodeete Nodes
-// Können je nach Bedarf geändert werden
-// Eventuell füge ich eine Funktion hinein die nach Klick die Nodes bestimmt
-const nodes = [
-    [100, 50],
-    [250, 100],
-    [400, 50],
-    [550, 100]
-];
 
+// function die max/min von X und Y wiedergibt
+// Kann nützlich für Testzwecke sein oder später
+// Hab gerade bisschen Kopfschmerzen, bestimmt lässt sich ein Nutzen finden    
+
+function gibMinMaxCoords(nodes) {
+    let minX = Math.min(...nodes.map(node => node[0]));
+    let minY = Math.min(...nodes.map(node => node[1]));
+    let maxX = Math.max(...nodes.map(node => node[0]));
+    let maxY = Math.max(...nodes.map(node => node[1]));
+
+    return {minX, minY, maxX, maxY};
+}
+
+// Kurzer Test ob die Koordinaten korrekt ausgegeben werden (ja werden sie)
+let coords = gibMinMaxCoords(nodes);
+console.log(coords); 
+
+// Funktion die eine schwarze Box abhängig von den Koordinaten zeichnet
+// Man sollte meinen CSS reicht dafür, jedoch ist der DIV kleiner als die jeweiligen Elemente drin
+// Lässt sich bestimmt optimieren => TODO? 
+
+function zeichneBox(nodes) {
+
+    const box = document.createElement('div');
+
+    let {minX, minY, maxX, maxY} = gibMinMaxCoords(nodes);
+
+    box.style.position = 'absolute';
+    box.style.left = minX + 'px';
+    box.style.top = minY + 'px';
+    box.style.width = maxX-minX + 'px';
+    box.style.height = maxY + 'px';
+
+    box.style.border = '2px solid black';
+
+    document.body.appendChild(box);
+
+}
+
+zeichneBox(nodes)
+
+//Zeichnet alle Kästchen zu guter Letzt
 nodes.forEach(nodes => drawKaestchen(...nodes));
